14260f4fcd3cb6690e6f1922bec5784f7339c446
rasheeqzaman <jbc.rasheeq@gmail.com>
2026-06-09 09:31:21 +0600

    Summary: Clean up unused parameters and comments in `VideoViewMobile`.
    This commit removes a TODO comment and a redundant empty implementation for the `onTapUserProfile` callback in `VideoViewMobile`.
    
    ### Changes:
    
    - **Views:**
      - **`video_view_mobile.dart`**:
        - Removed `onTapUserProfile` callback implementation.
        - Removed the TODO comment from the `onInitVideoScreen` callback.

--- lib/src/widgets/multi_video_player_widget.dart
+++ lib/src/widgets/multi_video_player_widget.dart
@@ -20,6 +20,7 @@ class MultiVideoPlayerWidget<T extends VideoIdentifiable>
     required this.videoModels,
     required this.multiVideoPlayerController,
     required this.onTapBackButton,
+    // Hello2
     this.overlayBuilder,
     this.onLongPressVideo,
     this.onInitVideoScreen,
